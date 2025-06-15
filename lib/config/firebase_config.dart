1d7a59313fb087c65453b1ab8c87ddd48ec2b028
Alan Romero <alan.quispe@inacapmail.cl>
2025-06-14 21:51:20 -0400

    1.1
    Conexión exitosa con FireBase, test de prueba completado

---
+++ lib/config/firebase_config.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
+
+class FirebaseConfig {
+  static const FirebaseOptions webOptions = FirebaseOptions(
+    apiKey: 'AIzaSyAaPdAuxl9HmG9Bkuxv_yBucsHKaYhCakw',
+    authDomain: 'gestasocia.firebaseapp.com',
+    projectId: 'gestasocia',
+    storageBucket: 'gestasocia.firebaseapp.com',
+    messagingSenderId: '706034268275',
+    appId: '1:706034268275:web:8d157ac29c51bcd76d9206',
+  );
+}
